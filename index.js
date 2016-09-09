3b8a24b85fa14eccc7e7a98d45c0c969911f6f7c
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2016-09-09 13:38:59 +0200

    adding index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+const lineNotInternal = /(^.*\((?!.*node_module.*|.*internal\/process.*|.*timers.*).*\))/m,
+mod = /node_modules\/(\w+)\//,
+errorLine = /^(.*Error.*)$/m;
+module.exports = stack => {
+    return stack.match(errorLine)[0] + '\n' +
+    (lineNotInternal.test(stack) ? lineNotInternal.exec(stack)[1] :'\tin node module ' + mod.exec(stack)[1]);
+}
